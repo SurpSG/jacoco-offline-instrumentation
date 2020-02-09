b472f943876aa58835df9671eee7b575ea395c1b
Sergii Gnatiuk <sergii.gnatiuk@worldapp.com>
2020-02-10 01:39:43 +0200

    added one more module

---
+++ 3rd/src/main/java/com/codeonly/Class3.java
@@ -0,0 +1,11 @@
+package com.codeonly;
+
+public class Class3 {
+
+    public int max( int a, int b ) {
+        if (a != b) {
+            return a > b ? a : b;
+        }
+        return a;
+    }
+}
--- code-only/src/main/java/com/codeonly/Class2.java
+++ code-only/src/main/java/com/codeonly/Class2.java
@@ -3,6 +3,6 @@ package com.codeonly;
 public class Class2 {
 
     public int max( int a, int b ) {
-        return a > b ? a : b;
+        return new Class3().max( a,b );
     }
 }
